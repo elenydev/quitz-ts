44c01410b1de5f799d84476e187a39dfa181df2b
Damian Czarnota <godevdamian@gmail.com>
2020-12-14 10:03:28 +0100

    added logic for initializing quiz, going through questions and restart quiz

--- src/pages/quiz/index.tsx
+++ src/pages/quiz/index.tsx
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { QUIZ_QUESTIONS } from '@/data/quizQuestions';
 
-const Quiz = (): React.ReactNode => (
-  <>
-    <h1>Hi :)</h1>
-  </>
-);
+import Button from '@material-ui/core/Button';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Quiz = (): React.ReactNode => {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [answers, setAnswer] = useState([]);
+
+  const setNextQuestion = (): void => {
+    setCurrentQuestion(currentQuestion + 1);
+  };
+
+  const restartQuiz = (): void => {
+    setCurrentQuestion(0);
+  };
+
+  return (
+    <Wrapper>
+      {currentQuestion <= QUIZ_QUESTIONS.length && (
+        <>
+          <h2>Quiz</h2>
+
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={setNextQuestion}
+          >
+            Next
+          </Button>
+        </>
+      )}
+      {currentQuestion > QUIZ_QUESTIONS.length && (
+        <>
+          <h3>Your score is:</h3>
+
+          <Button variant="contained" color="secondary" onClick={restartQuiz}>
+            Try again
+          </Button>
+        </>
+      )}
+    </Wrapper>
+  );
+};
 
 export default Quiz;
