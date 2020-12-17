7edfadbfcb0c728bb601a0cde8751682be56fdd2
Damian Czarnota <godevdamian@gmail.com>
2020-12-17 11:23:20 +0100

    added rules for gitignore

--- quitz-ts-server/src/constants.ts
+++
@@ -1,2 +0,0 @@
-export const mongoURI =
-  "mongodb+srv://damian:JIpFoq6kjWVbBL0K@cluster0.oq9ad.mongodb.net/quitz?retryWrites=true&w=majority";
--- quitz-ts-server/src/index.ts
+++ quitz-ts-server/src/index.ts
@@ -1,14 +1,15 @@
-import express from "express";
-import mongoose from "mongoose";
-import bodyParser from "body-parser";
-import cors from "cors";
-import methodOverride from "method-override";
-import path from "path";
-import dotenv from "dotenv";
-import { mongoURI } from "./constants";
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import methodOverride from 'method-override';
+import path from 'path';
+import dotenv from 'dotenv';
+import { mongoURI } from './constants/constants';
 
-//Routes
-import questionsRoutes from "./routes/questionsRoutes";
+// Routes
+
+import questionsRoutes from './routes/questionsRoutes';
 
 dotenv.config();
 
@@ -16,28 +17,28 @@ const router = express.Router();
 const app = express();
 
 app.use(cors());
-app.use(methodOverride("_method"));
+app.use(methodOverride(`_method`));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(`Access-Control-Allow-Origin`, `*`);
   res.setHeader(
-    "Access-Control-Allow-Methods",
-    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+    `Access-Control-Allow-Methods`,
+    `OPTIONS, GET, POST, PUT, PATCH, DELETE`,
   );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  if (req.method === "OPTIONS") {
+  res.setHeader(`Access-Control-Allow-Headers`, `Content-Type, Authorization`);
+  if (req.method === `OPTIONS`) {
     return res.sendStatus(200);
   }
   next();
 });
 
-router.get("/", (req, res, next) => {
-  res.sendFile(path.join(__dirname + "/index.html"));
+router.get(`/`, (req, res, next) => {
+  res.sendFile(path.join(`${__dirname}/index.html`));
 });
 
-app.use("/", router);
-app.use("/questions", questionsRoutes);
+app.use(`/`, router);
+app.use(`/questions`, questionsRoutes);
 
 mongoose
   .connect(mongoURI, {
